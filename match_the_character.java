2352878b98dff7e46e3558c7eedf712579c63425
Hamza <hrao01475@gmail.com>
2024-06-01 11:02:58 +0500

    Add Program of match the each character

---
+++ match_the_character.java
@@ -0,0 +1,23 @@
+class Match_the_charcter {
+    public static void main(String args[]){
+
+        String password = "Hamza@123"; // Enter any pasword
+        char[] crack_password = new char[password.length()];
+
+        //Check the character one-by-one
+
+        for(int i = 0; i < password.length(); i++){
+            
+                for(int k = 0; k <= 255; k++){
+                    if(password.charAt(i) == (char)k){  // If the charcter match then store in password
+                        crack_password[i] = (char)k;
+                        break;
+                    }
+                
+            }
+        }
+        for(int i = 0; i < crack_password.length; i++){
+            System.out.print(crack_password[i]);
+        } 
+    }
+}
