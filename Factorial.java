0ff59485019004669084688497a8c43201c4ec5b
Hamza <hrao01475@gmail.com>
2024-06-02 09:53:17 +0500

    Add code lines in Factorial file.

--- Factorial.java
+++ Factorial.java
@@ -3,5 +3,20 @@ import java.util.*;
 public class Factorial {
     public static void main(String args[]){
 
+        Scanner sc = new Scanner(System.in);
+
+        double num;
+        double result = 1;
+
+        System.out.print("Enter any number : ");
+        num = sc.nextDouble();
+
+        for(int i = 1; i <= num; i++ ){
+            result = i * result;
+        }
+
+        System.out.println("The Factorial of the number is : " + result);
+
+        sc.close();
     }
 }
