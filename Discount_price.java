6c7f1d3001cf789f26d0f9f2a468911a76c0ba26
Hamza <hrao01475@gmail.com>
2024-06-02 10:21:29 +0500

    Add comments in the Discount_price.java

--- Discount_price.java
+++ Discount_price.java
@@ -6,6 +6,8 @@ public class Discount_price {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
+        // Declare all the variables that is used in the program
+
         double amount;
         double discount = 0;
         double final_amount;
@@ -14,6 +16,7 @@ public class Discount_price {
         amount = sc.nextInt();
 
         // Discount Conditions
+        // We cannot add switch operator b/c in switch operation we can not pass condition like this (amount >= 500000).
 
         if(amount >= 500000){
             discount = amount * 0.15;
@@ -23,6 +26,8 @@ public class Discount_price {
             discount = amount * 0.05;
         }
             final_amount = amount - discount;
+
+            // Print the discount and Final Amount
 
             System.out.println("discount = " + discount);
             System.out.println("Total Price = " + final_amount);
