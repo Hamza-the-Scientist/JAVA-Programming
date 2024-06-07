62bbcbaf38806ef4a79fb84a1d4eaa3916737f9a
Hamza <hrao01475@gmail.com>
2024-06-07 10:26:37 +0500

    Add program of Print Rectangle of star

--- print_rectangle.java
+++ print_rectangle.java
@@ -1,5 +1,20 @@
+import java.util.*;
+
 class print_rectangle {
     public static void main(String args[]){
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of row to print \"*\" : ");
+        int row = sc.nextInt() ;
+        System.out.print("Enter number of column to print \"*\" : ");
+        int col = sc.nextInt();
+
+        for (int i = 0; i < row; i++){
+            System.out.println();            // For print Next line
+            for(int j = 0; j < col; j++){
+                System.out.print("*");
+            }
+        }
+
     }    
 }
