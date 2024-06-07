68fd385823a4eced384efc75f2370c85e5c9fb25
Hamza <hrao01475@gmail.com>
2024-06-07 10:51:14 +0500

    Add logic with codes of line in hollow_rectangle.java

--- hollow_rectangle.java
+++ hollow_rectangle.java
@@ -1,6 +1,29 @@
+// Print the hollow Rectangle
+import java.util.Scanner;
+
 class hollow_rectangle {
     public static void main(String arhs[]){
         
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of row to print \"*\" : ");    // Taking input of row
+        int row = sc.nextInt() ;
+        System.out.print("Enter number of column to print \"*\" : ");   // Takingn input of column
+        int column = sc.nextInt();
+
+        //outer loop
+        for (int i=1; i<=row; i++){
+            //inner loop
+            for(int j=1; j<=column; j++){
+                if(i == 1 || j == 1 || i == row || j == column){ // Giving conditions
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
     }
     
 }
