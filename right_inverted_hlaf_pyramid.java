a538669fd42a22e1632f0927f97b2fed6c7bed71
Hamza <hrao01475@gmail.com>
2024-06-08 11:37:42 +0500

    add logic to right_half_pyramid.java

--- right_inverted_hlaf_pyramid.java
+++ right_inverted_hlaf_pyramid.java
@@ -1,5 +1,20 @@
 class right_inverted_hlaf_pyramid {
     public static void main(String[] args) {
+
+        int space = 4;
+        // Outer Loop
+        for(int i = 1; i <=5; i++){
+            // Inner loop -->  Print space
+            for(int j=1; j<=space; j++){
+                System.out.print(" ");
+            }
+            // Inner loop --> Print Star
+            for(int k=1; k<=i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+            space--;
+        }
         
     }    
 }
