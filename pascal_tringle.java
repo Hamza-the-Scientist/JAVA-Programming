212cc01d13de337a189c99408996bc344f36b73a
Hamza <hrao01475@gmail.com>
2024-06-09 13:08:13 +0500

    Add pascal tringle pattern

---
+++ pascal_tringle.java
@@ -0,0 +1,20 @@
+class pascal_tringle {
+    public static void main(String[] args) {
+        
+        int n=5;
+
+        for(int i=0; i < n; i++){
+            int number = 1;
+
+            for(int k=1; k< n-i; k++){
+                System.out.print(" ");
+            }
+
+            for(int j=0; j <= i; j++){
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            } 
+            System.out.println();
+        }
+    }
+}
