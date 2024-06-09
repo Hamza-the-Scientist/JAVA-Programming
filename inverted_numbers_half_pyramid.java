fe023fa8e1fe085ec75624da6d702b8e80bcca0e
Hamza <hrao01475@gmail.com>
2024-06-09 10:43:21 +0500

    Add codes in inveretd half numbers pyramid.java

--- inverted_numbers_half_pyramid.java
+++ inverted_numbers_half_pyramid.java
@@ -1,5 +1,13 @@
 class inverted_numbers_half_pyramid {
     public static void main(String[] args) {
         
+        // Outer loop
+        for(int i=5; i>=1; i--){
+            //Inner loop
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }    
 }
